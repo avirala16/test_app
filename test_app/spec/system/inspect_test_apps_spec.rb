6eeb6ac48940f95381b5bc7ec2122df06d1cf6f4
root <root@localhost>
2024-02-06 02:09:57 +0000

    CI changes

---
+++ test_app/spec/system/inspect_test_apps_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "InspectTestApps", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  it 'adding a new book' do
+    visit '/books/new'
+    fill_in 'Title', with: 'Hello!'
+    click_on 'Create Book'
+
+    expect(page).to have_content('Book was successfully created.')
+
+    book = Book.order("id").last
+    expect(book.title).to eq('Hello!')
+  end
+end
